76f159d86db65b5c45a3993c41ba6a1c10d5623f
Victoria Kay <vkay@mymail.mines.edu>
2014-04-15 17:09:00 -0600

    Finished basic layout

--- Main.java
+++ Main.java
@@ -1,4 +1,6 @@
+import java.awt.BorderLayout;
 import java.awt.Graphics;
+import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.MediaTracker;
 import java.awt.Toolkit;
@@ -13,29 +15,14 @@ public class Main extends JFrame {
 	public Main() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setTitle("Final Project");
+		//setLayout(new GridLayout(1,2));
 		setSize(800, 600);
-		JPanel img = new ImagePanel();
-		add(img);
-	}
-	
-	public class ImagePanel extends JPanel {
-		private Image thumbnail;
-		public ImagePanel() {
-			MediaTracker tracker = new MediaTracker(this);
-			URL url = getClass().getResource("/Images/forestmap.jpg");
-			Image original = Toolkit.getDefaultToolkit().getImage(url);
-			tracker.addImage(original, 0);
-			try {
-				tracker.waitForID(0);
-			} catch (InterruptedException e) { 
-				return; 
-			}
-			thumbnail = original.getScaledInstance(600, 600,  Image.SCALE_FAST);
-		}
-		public void paintComponent(Graphics g) {
-		    int PADDING = 0;
-		    g.drawImage(thumbnail, PADDING, PADDING, 600, 600, this);
-		}
+		ImagePanel img = new ImagePanel();
+		img.setSize(600,600);
+		add(img, BorderLayout.WEST);
+		IconPanel icons = new IconPanel();
+		icons.setSize(200,600);
+		add(icons, BorderLayout.EAST);
 	}
 
 	public static void main(String[] args) {
@@ -43,8 +30,8 @@ public class Main extends JFrame {
 	        public void run() {
 	        	Main gui = new Main(); 
 	    		gui.setVisible(true);
-	        }  });
-
+	        }  
+	    });
 	}
 	
 }
